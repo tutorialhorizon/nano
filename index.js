596431c54a3b995590608af7a17afd1f5097c5be
thmain <tutorialhorizon@gmail.com>
2016-01-15 08:22:00 -0800

    Added more methods

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 module.exports = {
-	getQueryParams: require('./lib/getQueryParams')
+	getQueryParams: require('./lib/getQueryParams'),
+	arrayToMap: require('./lib/arrayToMap'),
+	getQueryString: require('./lib/getQueryString'),
+	getHostNameFromUrl: require('./lib/getHostNameFromUrl'),
+	reloadPage: require('./lib/reloadPage'),
+	excludeFromList: require('./lib/excludeFromList')
 };
